22d8f0f0d6eda0d6a8b66246787a77262f6baf5e
White <jinhao.zhang@icloud.com>
2017-08-10 23:37:27 +0800

    Initial Commit

---
+++ Algorithms4InSwift/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Algorithms4InSwift
+//
+//  Created by White on 2017/8/10.
+//  Copyright © 2017年 White. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
